0688086d6b1fefa4fa7216af4da315b15d7e5c2f
Hideto Yuzawa <hiyuzawa@gmail.com>
2014-12-08 11:09:36 +0900

    namespace の利用

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ function handler(req, res) {
 	})
 }
 
-io.sockets.on("connection", function(socket){
+var chat = io.of("/chat").on("connection", function(socket){
+	console.log("connection : /chat")
 	socket.on("emit_from_client", function(data){
 		console.log(data)
 		socket.join(data.room)
@@ -23,3 +24,9 @@ io.sockets.on("connection", function(socket){
 		socket.broadcast.to(data.room).emit("emit_from_server", data.msg)
 	})
 })
+
+var news = io.of("/news").on("connection", function(socket){
+	console.log("connection : /news")
+	socket.emit("emit_from_server", "Today: " + new Date())
+})
+
